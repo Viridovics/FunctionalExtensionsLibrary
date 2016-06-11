1df6366dc3feb915add59b051f6ca5e82c0dba71
Viridovics <m_ec@mail.ru>
2016-06-11 20:45:58 +0300

    Create test project

--- FunctionalExtensionsLibrary/Monads/SimpleMonadsExtensions.cs
+++ FunctionalExtensionsLibrary/Monads/SimpleMonadsExtensions.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FunctionalExtensionsLibrary.Monads
 {
 	public static class SimpleMonadsExtensions
 	{
-		public static T IfNotNull<T,V>(this V value, Func<V,T> action)
-																		where V:class
-																		where T:class
+		/// <summary>
+		/// TODO
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="V"></typeparam>
+		/// <param name="value"></param>
+		/// <param name="action"></param>
+		/// <returns></returns>
+		public static T IfNotNull<T, V>(this V value, Func<V, T> action)
+			where V : class
+			where T : class
 		{
-			if(value == null)
+			if (value == null)
 			{
 				return null;
 			}
---
+++ Tests/Monads/MaybeMonadTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FunctionalExtensionsLibrary.Monads;
+
+namespace Tests.Monads
+{
+	/// <summary>
+	/// Test IfNotNull extension or 'maybe' monad
+	/// </summary>
+	[TestClass]
+	public class MaybeMonadTest
+	{
+		[TestMethod]
+		public void InstanceIsNull()
+		{
+			Cow cow = null;
+			var cowAnswer = cow.IfNotNull(c => c.WhatCowSay("How are you?"));
+			Assert.IsNull(cowAnswer);
+		}
+
+		[TestMethod]
+		public void InstanceIsNotNull()
+		{
+			var cow = new Cow();
+			var yourQuestion = "How are you?";
+			var cowAnswer = cow.IfNotNull(c => c.WhatCowSay(yourQuestion));
+			Assert.AreEqual(cowAnswer, cow.WhatCowSay(yourQuestion));
+		}
+	}
+
+	internal class Cow
+	{
+		public string WhatCowSay(string yourQuestion)
+		{
+			return string.Format("Your question: {0}. But cow say: MOOOOOO!", yourQuestion);
+		}
+	}
+}
