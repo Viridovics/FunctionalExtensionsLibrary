3cf0ad7480f805b7bce9229c694ff288a311a2ca
Viridovics <m_ec@mail.ru>
2016-06-11 20:29:31 +0300

    Create master project

---
+++ FunctionalExtensionsLibrary/Monads/SimpleMonadsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunctionalExtensionsLibrary.Monads
+{
+	public static class SimpleMonadsExtensions
+	{
+		public static T IfNotNull<T,V>(this V value, Func<V,T> action)
+																		where V:class
+																		where T:class
+		{
+			if(value == null)
+			{
+				return null;
+			}
+			return action(value);
+		}
+	}
+}
